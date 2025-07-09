d550b017eb79f3018675b0f6dacdcda08e61a604
98kwcho <54492494+98kwcho@users.noreply.github.com>
2025-07-09 12:09:37 +0900

    operator assignment modifiy

--- python/example02-8.py
+++ python/example02-8.py
@@ -1,6 +1,8 @@
 height = int(input("키를 입력해 주세요."))
 weight = int(input("몸무게를 입력해 주세요."))
 
+#weight, height = eval(input("키 몸무게 입력"))
+
 avr_weight = (height - 100) * 0.9
 
 if weight > avr_weight:
--- python/example02-9.py
+++ python/example02-9.py
@@ -1,38 +1,69 @@
 import random
 
-operator = ["+", "-", "*", "//"]
+# random.random()
+# random.randrange(0, 5)
+# random.randint(0, 10)
+
+operator = ["+", "-", "*", "/"]
 choiesOperator = random.randint(0, 3)
-ranNum1 = random.randint(0, 100)
-ranNum2 = random.randint(0, 100)
+ranNum1 = random.randint(1, 100)
+ranNum2 = random.randint(1, 100)
 
 print(f"{ranNum1}{operator[choiesOperator]}{ranNum2} = ?")
 
-inputRes = int(input("답"))
+inputRes = input("답")
 
 if choiesOperator == 0:
-    if(inputRes == (ranNum1 + ranNum2) ):
+    if(int(inputRes) == (ranNum1 + ranNum2) ):
         print("정답")
 
     else :
         print("오답")
 
 elif choiesOperator == 1:
-    if(inputRes == (ranNum1 - ranNum2) ):
+    if(int(inputRes) == (ranNum1 - ranNum2) ):
         print("정답")
 
     else :
         print("오답")
 
 elif choiesOperator == 2:
-    if(inputRes == (ranNum1 * ranNum2) ):
+    if(int(inputRes) == (ranNum1 * ranNum2) ):
         print("정답")
 
     else :
         print("오답")
 
 else :
-    if(inputRes == (ranNum1 // ranNum2) ):
+    if(float(inputRes) == (ranNum1 / ranNum2) or ((ranNum1 / ranNum2)-float(inputRes)) < 0.01):
         print("정답")
 
     else :
         print("오답")
+
+# 3.10 이후 버전에서 사용하는 match case 
+'''match choiesOperator:   
+    case 0:
+        if(int(inputRes) == (ranNum1 + ranNum2) ):
+            print("정답")
+
+        else :
+            print("오답")
+    case 1:
+        if(int(inputRes) == (ranNum1 - ranNum2) ):
+            print("정답")
+
+        else :
+            print("오답")
+    case 2:
+        if(int(inputRes) == (ranNum1 * ranNum2) ):
+            print("정답")
+
+        else :
+            print("오답")                     
+    case 3:
+        if(float(inputRes) == (ranNum1 / ranNum2) or ((ranNum1 / ranNum2)-float(inputRes)) < 0.01):
+          print("정답")
+
+        else :
+            print("오답")'''
