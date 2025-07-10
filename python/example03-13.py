b8c54067a3fe0f65c60a646efe6eae81b42a14e1
98kwcho <54492494+98kwcho@users.noreply.github.com>
2025-07-10 09:25:37 +0900

    함수 없이 피보나치

--- python/example03-13.py
+++ python/example03-13.py
@@ -1,11 +1,6 @@
-def fib(n):
-    a, b = 0, 1
-    for _ in range(n):
-        a, b = b, a + b 
-    return a
+inputNum = int(input("정수를 입력하시오: "))
 
-inputNum = int(input("정수를 입력하시오"))
-
+a, b = 0, 1
 for i in range(inputNum):
-	print(fib(i)) 
-     
+    print(a, end= " ")
+    a, b = b, a + b
