714edb87073b4ab94e8a4fe044fd4eb5772ee641
98kwcho <54492494+98kwcho@users.noreply.github.com>
2025-07-10 09:07:11 +0900

    오류 수정

--- python/example03-8.py
+++ python/example03-8.py
@@ -1,10 +1,11 @@
-def fibo(n):	
-    if n == 1 or n == 2:
-        return 1
-    else:
-    	return fibo(n-1) + fibo(n-2)
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b 
+    return a
 
 inputNum = int(input("정수를 입력하시오"))
 
 for i in range(inputNum):
-	print(fibo(i)) 
+	print(fib(i)) 
+     
